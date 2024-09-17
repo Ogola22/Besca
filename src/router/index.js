fb1f090cf2cc2bd2bb1fc5a29efe614ff70ff87b
brian_ogola <brianochiengogola78@gmail.com>
2024-09-17 12:47:31 +0300

    Index.js routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,44 +1,63 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import AboutView from '@/views/AboutView.vue';
+import ContactView from '@/views/ContactView.vue';
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'About',
-      component: () => import('@/views/AboutView.vue')
-    },
-    {
-      path: '/contact',
-      name: 'Contact',
-      component: () => import('@/views/ContacView.vue')
-    },
-    {
-      path: '/solarPV',
-      name: 'SolarPV',
-      component: () => import('@/views/SolarPV.vue')
-    },
-    {
-      path: '/projects',
-      name: 'Projects',
-      component: () => import('@/views/Projects.vue')
-    },
-    {
-      path: '/community',
-      name: 'Community',
-      component: () => import('@/views/TransformingLives.vue')
-    },
-  ];
-  const: router = createRouter({
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: AboutView
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: ContactView
+  }
+]
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: HomeView
+//     },
+//     {
+//       path: '/about',
+//       name: 'About',
+//       component: () => import('@/views/AboutView.vue')
+//     },
+//     {
+//       path: '/contact',
+//       name: 'Contact',
+//       component: () => import('@/views/ContacView.vue')
+//     },
+//     {
+//       path: '/solarPV',
+//       name: 'SolarPV',
+//       component: () => import('@/views/SolarPV.vue')
+//     },
+//     {
+//       path: '/projects',
+//       name: 'Projects',
+//       component: () => import('@/views/Projects.vue')
+//     },
+//     {
+//       path: '/community',
+//       name: 'Community',
+//       component: () => import('@/views/TransformingLives.vue')
+//     },
+//   ];
+  const router = createRouter({
     history: createWebHistory(), // This enables history mode
-    router
+    routes
   })
-});
+
 
 export default router
