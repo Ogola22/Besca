c50202408ad018bda9c5e4906bad5ce6ddcf0e57
brian_ogola <brianochiengogola78@gmail.com>
2024-09-17 12:21:20 +0300

    Transformlive images

--- src/views/TransformingLives.vue
+++ src/views/TransformingLives.vue
@@ -40,7 +40,7 @@
                         <div class="blog-img">
                             <img src="../assets/img/blog/blog-1-1.jpg" alt="Blog Image" class="w-100">
                         </div>
-                        <h3 class="blog-title"><a href="blog-details.html">Solar PV Installation at </a></h3>
+                        <h6 class="blog-title"><a href="blog-details.html">Solar PV Installation at Olekimaki Primary School in Kajiado </a></h6>
                         <div class="blog-content">
                             <a href="blog-details.html" class="link-btn">Read More<i class="far fa-plus"></i></a>
                         </div>
@@ -50,9 +50,9 @@
                 <div class="col-xl-4 wow fadeInUp" data-wow-delay="0.4s">
                     <div class="vs-blog blog-style1">
                         <div class="blog-img">
-                            <img src="../assets/img/blog/blog-1-2.jpg" alt="Blog Image" class="w-100">
+                            <img src="../assets/img/blog/transform3.jpg" alt="Blog Image" class="w-100">
                         </div>
-                        <h3 class="blog-title"><a href="blog-details.html">Solar PV Installation</a></h3>
+                        <h6 class="blog-title"><a href="blog-details.html">Solar PV Installation</a></h6>
                         <div class="blog-content">
                             <a href="blog-details.html" class="link-btn">Read More<i class="far fa-plus"></i></a>
                         </div>
@@ -62,9 +62,9 @@
                 <div class="col-xl-4 wow fadeInUp" data-wow-delay="0.5s">
                     <div class="vs-blog blog-style1">
                         <div class="blog-img">
-                            <img src="../assets/img/blog/blog-1-3.jpg" alt="Blog Image" class="w-100">
+                            <img src="../assets/img/blog/transform4.jpg" alt="Blog Image" class="w-100">
                         </div>
-                        <h3 class="blog-title"><a href="blog-details.html">Solar PV Installation</a></h3>
+                        <h6 class="blog-title"><a href="blog-details.html">Solar PV Installation</a></h6>
                         <div class="blog-content">
                             <a href="blog-details.html" class="link-btn">Read More<i class="far fa-plus"></i></a>
                         </div>
